b262fd51273ce053bc0dd9d0c54b5483e6894c28
md jakir hossen <hossenm3@gmail.com>
2020-10-18 13:33:55 -0400

    hw

---
+++ src/odyssey/java/assignments/loops/Exercise_1_naturalNumber.java
@@ -0,0 +1,14 @@
+package src.odyssey.java.assignments.loops;
+
+import javax.crypto.spec.PSource;
+
+public class Exercise_1_naturalNumber {
+    public static void main(String[] args) {
+        System.out.println("Ten natural numbers are: ");
+
+        for(int num1 = 1; num1<=10;num1++){
+
+            System.out.print(" "+num1);
+        }
+    }
+}
